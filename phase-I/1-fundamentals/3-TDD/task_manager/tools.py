7c6ca798048f0b39415c192724f6eb2c4c060d1d
kevin beier <kabeier@gmail.com>
2026-07-01 15:00:20 -0400

    after peer programming demo

--- phase-I/1-fundamentals/3-TDD/task_manager/tools.py
+++ phase-I/1-fundamentals/3-TDD/task_manager/tools.py
@@ -1,2 +1,4 @@
+from db import generate_id
+
 def create_task():
-    return {'id':1,'title':input("What title do you want for your task?\n"), 'completed':False}
+    return {'id':generate_id(),'title':input("What title do you want for your task?\n"), 'completed':False}
