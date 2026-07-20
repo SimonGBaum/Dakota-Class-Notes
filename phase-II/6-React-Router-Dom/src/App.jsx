3542b73629a3187db91679d21d52f38ba0698cf4
kevin beier <kabeier@gmail.com>
2026-07-20 12:40:12 -0400

    end of intro to react-router added navbar

--- phase-II/6-React-Router-Dom/src/App.jsx
+++ phase-II/6-React-Router-Dom/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Heading from "./components/Heading";
+import NavBar from './components/NavBar';
 
 function App() {
   const [user, setUser ] =useState(null)
@@ -20,15 +21,18 @@ function App() {
 
 
   return (
-    <main className="min-h-screen bg-slate-100 p-8">
-      <div className="mx-auto max-w-5xl space-y-8 ">
-        <Heading/>
-        <Outlet/>
-        <button onClick={()=>{setUser("Neil Patrick Harris")}}>
-          Set user
-        </button>
-      </div>
-    </main>
+    <>
+      <NavBar/>
+      <main className="min-h-screen bg-slate-100 p-8">
+        <div className="mx-auto max-w-5xl space-y-8 ">
+          <Heading/>
+          <Outlet/>
+          <button onClick={()=>{setUser("Neil Patrick Harris")}}>
+            Set user
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
 
---
+++ phase-II/6-React-Router-Dom/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+    // <ul>
+    //     <li>
+    //         <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //         <Link to="/about">About</Link>
+    //     </li>
+    // </ul>
+
+export default function NavBar() {
+  return (
+
+    <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
+  )
+}
