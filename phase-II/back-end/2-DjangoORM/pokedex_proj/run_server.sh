43f37f8bf343d460e9fa773835554569889e936b
kevin beier <kabeier@gmail.com>
2026-07-28 15:20:29 -0400

    after part 2 of orm lecture

--- phase-II/back-end/2-DjangoORM/pokedex_proj/run_server.sh
+++ phase-II/back-end/2-DjangoORM/pokedex_proj/run_server.sh
@@ -5,4 +5,9 @@ docker run --rm \
 -p 8000:8000 \
 --name django-container \
 --network pokedex-network \
+--user "$(id -u):$(id -g)" \
 django-img
+
+#  The line:
+#  --user "$(id -u):$(id -g)" \
+#  is just for Linux users to avoid file permission issues
