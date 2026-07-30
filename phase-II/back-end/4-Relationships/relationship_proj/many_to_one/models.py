a5b3ea86509acefadb6bca8e6401a036dcd266d2
kevin beier <kabeier@gmail.com>
2026-07-30 12:13:21 -0400

    end of the relationship project

--- phase-II/back-end/4-Relationships/relationship_proj/many_to_one/models.py
+++ phase-II/back-end/4-Relationships/relationship_proj/many_to_one/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class LuggageBag(models.Model):
+    weight = models.IntegerField(default=35)
+    passenger = models.ForeignKey(
+        to="one_to_one.Passenger",
+        on_delete=models.CASCADE,
+        related_name="luggage_bags"
+    )
+    
+    
+    def __str__(self):
+        return f"Owner: {self.passenger.name} | Weight: {self.weight} "
--- phase-II/back-end/4-Relationships/relationship_proj/one_to_one/admin.py
+++ phase-II/back-end/4-Relationships/relationship_proj/one_to_one/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Passenger, Passport
 
 # Register your models here.
+admin.site.register([Passenger, Passport])
