1a5719721e98af435e4303cdf714e5c5954f05cf
kevin beier <kabeier@gmail.com>
2026-07-30 15:18:10 -0400

    pre part 2 realtionships

--- phase-II/back-end/4-Relationships/pokedex_proj/pokedex_proj/urls.py
+++ phase-II/back-end/4-Relationships/pokedex_proj/pokedex_proj/urls.py
@@ -14,9 +14,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# FUNCTION BASED VIEWS
+# ACCEPT REQUEST & PARAMS
+# ALWAYS RETURN A RESPONSE
+
+def hello(request):
+    print(request.method)
+    print(request.path)
+    print(request.headers)
+    # uses <pre>f"{request.SOEMTHING here}"</pre>
+
+    return HttpResponse("Hello World")
+
+def area_square(request, width):
+    return HttpResponse(width**2)
+
+import math
+def area_circle(request, radius):
+    math.pi * (radius**2)
+    # Return the math frist then show about the part of the request.headers and teh request.body
+    return HttpResponse(f"{request.body}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
+    path('square/<int:width>/', area_square),
+    path('circle/<int:radius>/', area_circle)
 ]
