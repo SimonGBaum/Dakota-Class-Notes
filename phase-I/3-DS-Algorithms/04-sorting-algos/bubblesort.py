f7f94bdc57494ca5063e6ab0c13dae9bd519798c
kevin beier <kabeier@gmail.com>
2026-07-31 10:59:01 -0400

    end of Sorting LAgo lecture

--- phase-I/3-DS-Algorithms/04-sorting-algos/bubblesort.py
+++ phase-I/3-DS-Algorithms/04-sorting-algos/bubblesort.py
@@ -15,3 +15,24 @@ bubble_sort(data)
 print(data)
 bubble_sort(sorted_data)
 print(sorted_data)
+
+
+
+def bubble_sort2(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped=True
+        if not swapped:
+            break
+                
+                
+data = [4, 3, 2, 1]
+sorted_data = [1, 2, 3, 4]
+bubble_sort2(data)
+print(data)
+bubble_sort2(sorted_data)
+print(sorted_data)
---
+++ phase-I/3-DS-Algorithms/04-sorting-algos/insertionsort.py
@@ -0,0 +1,15 @@
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        item = arr[i]
+        j = i-1
+        while j>= 0 and item < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = item
+        
+    return arr
+
+arr = [1, 7, 4, 3]
+insertion_sort(arr)
+print(arr)
